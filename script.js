15161da67cba2fc8f145f3f390ec6606e6392399
Andy Krause <andre.krause95@gmail.com>
2018-01-23 06:47:00 +0100

    - Visual change and IP-Symcon implementation

--- script.js
+++ script.js
@@ -10,16 +10,81 @@ window.onload = function() {
 	}
 }
 
-function buttonClicked(object) {
+function myLoop () {
+   setTimeout(function () {
+	   	var buttonsArray = document.getElementsByClassName('button');
+		var sliderArray = document.getElementsByClassName('sliderBox');
+		for(var i=0; i<buttonsArray.length; i++) {
+			symconID = buttonsArray[i].getAttribute('statusID');
+			if(symconID > 0) {
+				symconResponse = symconSet('GetValue', [parseInt(symconID, 10)]);
+				responseParsed = JSON.parse(symconResponse);
+				value = 'off';
+				if(responseParsed.result == true) {
+					value = 'on';
+				}
+				buttonsArray[i].setAttribute('status', value);
+			}
+		}
+		for(var i=0; i<sliderArray.length; i++) {
+			symconID = sliderArray[i].getAttribute('statusID');
+			if(symconID > 0) {
+				symconResponse = symconSet('GetValue', [parseInt(symconID, 10)]);
+				responseParsed = JSON.parse(symconResponse);
+				percentage = responseParsed.result * 100;
+				setSlider(sliderArray[i], percentage);
+			}
+		}
+		myLoop();
+   }, 1000)
+}
+myLoop();
+
+function symconSet(command, parameter) {
+	var url = '/api/';
+	var username = 'funkyandy1@gmail.com';
+	var password = 'symcon';
+	
+	var HTTP = new XMLHttpRequest();
+	HTTP.open('POST', url, false);
+	HTTP.onreadystatechange = function () {
+		if (HTTP.readyState == 4 && HTTP.status == 200) {
+			return HTTP.response;
+		}
+	}
+	
+	HTTP.setRequestHeader('Content-type', 'text/plain; charset=utf-8');
+	HTTP.setRequestHeader('Access-Control-Allow-Origin', '*');
+	HTTP.setRequestHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-token');
+	HTTP.setRequestHeader('Authorization', 'Basic ' + btoa(username+':'+password));
+	
+	data = {
+		'jsonrpc' : '2.0',
+		'id' : 0,
+		'method' : command,
+		'params' : parameter,
+	}
+	var rpc = JSON.stringify(data);
+	HTTP.send(rpc);
+	return HTTP.onreadystatechange();
+}
+
+function buttonClicked(object, event) {
+	event.preventDefault();
+	
 	var audio = new Audio('click2.mp3');
-	audio.play();
+	//audio.play();
 
 	currentStatus = object.getAttribute('status');
 	if(currentStatus == 'on') {
 		object.setAttribute('status', 'off');
+		value = false;
 	} else {
 		object.setAttribute('status', 'on');
+		value = true;
 	}
+	symconID = object.getAttribute('symconID');
+	symconSet('HM_WriteValueBoolean', [parseInt(symconID, 10), 'STATE', value]);
 }
 
 function sliderClicked(event, object) {
@@ -36,21 +101,37 @@ function sliderClicked(event, object) {
 	var sliderBoxClickBottom = sliderBoxBound.bottom - sliderBoxBorder - sliderBoxPadding;
 	var sliderBoxClickSize = sliderBoxClickBottom - sliderBoxClickTop;
 	
-	var sliderBar = object.getElementsByClassName('sliderBar')[0];
-	var sliderText = object.getElementsByClassName('sliderText')[0];
 	var sliderPercentOld = parseInt(object.getAttribute('value'));
 	
 	if(clickY <= sliderBoxClickTop) {
-		//relativePercent = 100;
 		relativePercent = sliderPercentOld + 5;
 	} else if (clickY >= sliderBoxClickBottom) {
-		//relativePercent = 0;
 		relativePercent = sliderPercentOld - 5;
 	} else  {
 		relativePercent = (sliderBoxClickBottom - clickY) * 100 / sliderBoxClickSize;
 	}
 	
-	var relativePercentRounded = Math.round(relativePercent / 5) * 5;
+	if((sliderPercentOld <= 0 && relativePercent > 0) || (sliderPercentOld > 0 && relativePercent <= 0)) {
+		//audio.play();
+	}
+	setSlider(object, relativePercent);
+}
+
+function sliderSend(object) {
+	var sliderPercentOld = parseInt(object.getAttribute('value'));
+	symconID = object.getAttribute('symconID');
+	symconSet('HM_WriteValueFloat', [parseInt(symconID, 10), 'LEVEL', sliderPercentOld / 100]);
+}
+
+function sliderGhost(event, object) {
+	
+}
+
+function setSlider(object, value) {
+	var sliderBar = object.getElementsByClassName('sliderBar')[0];
+	var sliderText = object.getElementsByClassName('sliderText')[0];
+
+	var relativePercentRounded = Math.round(value / 5) * 5;
 	
 	if(relativePercentRounded > 0 ){
 		object.setAttribute('status', 'on');
@@ -63,21 +144,10 @@ function sliderClicked(event, object) {
 	} else if (relativePercentRounded >= 100) {
 		relativePercentRounded = 100;
 	}
-	
-	if((sliderPercentOld <= 0 && relativePercentRounded > 0) || (sliderPercentOld > 0 && relativePercentRounded <= 0)) {
-		audio.play();
-	}
+
 	sliderBar.style.height =  relativePercentRounded + '%';
 	sliderText.innerHTML = relativePercentRounded + '%';
 	object.setAttribute('value', relativePercentRounded);
-}
-
-function sliderGhost(event, object) {
-	
-}
-
-function sliderMouseDown(event, object) {
-	alert('Mouse Down!');
 }
 
 function sliderTouched(event, object) {
